38186355e99369264cfb67d7df31a0fb5d1555c8
11249a212-png <11249a212@kanchiuniv.ac.in>
2025-11-07 23:15:31 -0800

    U5-this Pointer — Account Deposit.cpp

---
+++ U5-this Pointer — Account Deposit.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class Account {
+    double balance;
+public:
+    Account(double b = 0.0) : balance(b) {}
+
+    void deposit(double balance) {   // parameter name same as member
+        this->balance += balance;    // use this-> to refer to data member
+    }
+
+    void show() const {
+        cout << "Balance: " << balance << endl;
+    }
+};
+
+int main() {
+    Account a(1000.0);
+    a.show();
+
+    a.deposit(500.0);
+    a.show();
+
+    return 0;
+}
